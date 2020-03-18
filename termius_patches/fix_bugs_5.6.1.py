33104939dab2d76836c0dd4f6277735c20d28d44
YYY <yyy3752@gmail.com>
2020-03-19 01:03:38 +0900

    5.6.1

---
+++ termius_patches/fix_bugs_5.6.1.py
@@ -0,0 +1,16 @@
+#!python3
+
+from patches import patches
+from patches import do_patch
+
+patches_list = {
+    ("js", "entry.js"): [patches.fix_duplicate_key_event_handling]
+}
+
+
+def main():
+    do_patch(patches_list)
+
+
+if __name__ == "__main__":
+    main()
